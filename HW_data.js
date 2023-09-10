80e2a617b0ed5f80ba6faebb1d4ef74cfed6f98d
Andrew <voress@yandex.ru>
2023-09-10 23:57:48 +0300

    Домашняя работа Урок 10. Семинар. Работа с JSON

---
+++ HW_data.js
@@ -0,0 +1,111 @@
+const dataInfo = 
+`[
+    {
+        "state-province": null,
+        "name": "Adnan Menderes University",
+        "web_pages": [
+            "http://www.adu.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "adu.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Ahi Evran University",
+        "web_pages": [
+            "http://www.ahievran.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "ahievran.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Akdeniz University",
+        "web_pages": [
+            "http://www.akdeniz.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "akdeniz.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Anadolu University",
+        "web_pages": [
+            "http://www.anadolu.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "anadolu.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Ankara University",
+        "web_pages": [
+            "http://www.ankara.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "ankara.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Istanbul Arel University",
+        "web_pages": [
+            "http://www.arel.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "arel.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Atatürk University",
+        "web_pages": [
+            "http://www.atauni.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "atauni.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Atilim University",
+        "web_pages": [
+            "http://www.atilim.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "atilim.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    },
+    {
+        "state-province": null,
+        "name": "Bahcesehir University",
+        "web_pages": [
+            "http://www.bahcesehir.edu.tr/"
+        ],
+        "country": "Turkey",
+        "domains": [
+            "bahcesehir.edu.tr"
+        ],
+        "alpha_two_code": "TR"
+    }
+]`
