4409d82f2868f9d8ebcb2f143fd954d256120fd7
Andrew <voress@yandex.ru>
2023-10-17 21:44:20 +0300

    Урок 4. Семинар: Методы и работа с контентом

---
+++ HW_200/article.js
@@ -0,0 +1,48 @@
+new Vue({
+    el: "#app",
+    data: {
+        articles: [{
+            src: "img/Article1.png",
+            articleN__text: "Let's Get Solution For Building Construction Work",
+            articleN__date: "26 December,2022",
+            design: "Kitchan Design",
+            lighting: ""
+        },
+        {
+            src: "img/Article2.png",
+            articleN__text: "Low Cost Latest Invented Interior Designing Ideas.",
+            articleN__date: "22 December,2022",
+            design: "Living Design",
+            lighting: "articleN__backgroundArticle"
+        },
+        {
+            src: "img/Article3.png",
+            articleN__text: "Best For Any Office & Business Interior Solution",
+            articleN__date: "25 December,2022",
+            design: "Interior Design",
+            lighting: ""
+        },
+        {
+            src: "img/Article4.png",
+            articleN__text: "Let's Get Solution For Building Construction Work",
+            articleN__date: "26 December,2022",
+            design: "Kitchan Design",
+            lighting: ""
+        },
+        {
+            src: "img/Article5.png",
+            articleN__text: "Low Cost Latest Invented Interior Designing Ideas.",
+            articleN__date: "22 December,2022",
+            design: "Living Design",
+            lighting: ""
+        },
+        {
+            src: "img/Article6.png",
+            articleN__text: "Best For Any Office & Business Interior Solution",
+            articleN__date: "25 December,2022",
+            design: "Interior Design",
+            lighting: ""
+        }
+        ]
+    }
+})
